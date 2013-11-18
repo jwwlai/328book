4dfefab3eee212dba7252cd1c783796cdc207a5e
David Gaskins <david@dot.(none)>
2013-11-18 21:44:20 +0000

    finished insertion sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -2,21 +2,26 @@
 #include <iostream>
 using namespace std;
 
-
-void insertionSort(vector<int> s){
+void printVector(vector<int> v){
+	for(int i = 0; i< v.size(); ++i){
+		cout<<v[i]<< " ";
+	}
+	cout<<endl;
+}
+void insertionSort(vector<int> *s){
 	
 	//must declare j outside of its for loop for variable scope reasons
 	int j;
 	
 	//while you can, push i as far left as possible
-	for(int i = 1; i< s.size(); ++i){
-		int tmp = s[i];
-		for(int j = i-1; j >0; --j){
-			if(s[j]> tmp)}
-				s[j+1] = s[j];
+	for(int i = 1; i< (*s).size(); ++i){
+		int tmp = (*s)[i];
+		for(j = i; j >0; --j){
+			if((*s)[j-1]> tmp)
+				(*s)[j] =(*s)[j-1];
 			else break;
 		}
-		s[j] = tmp;
+		(*s)[j] = tmp;
 	}
 }
 
@@ -33,8 +38,8 @@ int main(){
 	}
 	
 	//actually sort them
-	insertionSort(s);
+	insertionSort(&s);
 	
 	//print the vector
-	
+	printVector(s);	
 }
