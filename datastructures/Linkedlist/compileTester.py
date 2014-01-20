0a47263b2e28b01c59c4031529d9c08033b48c28
David Gaskins <david.s.gaskins@gmail.com>
2014-01-19 16:59:08 -0800

    restructuring

---
+++ datastructures/Linkedlist/compileTester.py
@@ -0,0 +1,4 @@
+import os
+out = os.system("javac LinkedListTester.java")
+if out== 0:
+	out = os.system("java LinkedListTester")
---
+++ datastructures/Linkedlist/linkedlist.h
@@ -0,0 +1,38 @@
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+	struct Node
+	{
+		int data;
+		struct Node* next;
+	};
+	class LinkedList
+	{
+		private:
+			Node* head;
+			int listSize;
+		public:
+			//constructors and destructor
+			LinkedList();
+			LinkedList(int val);
+			LinkedList(LinkedList & a);
+			LinkedList(int* list, int listSize);
+			~LinkedList();
+			//operators
+			LinkedList & operator = (LinkedList & a);
+			//getters
+			Node* getHead();
+			int getSize();
+			// methods that will alter the list
+			void insertAtEnd(int data);
+			void insertAtFront(int data);
+			void insertNth(int data, int position);
+			void mergeLists(LinkedList* headA);
+			void removeFront();
+			//utilities
+			int compareTo(LinkedList* headA);
+			LinkedList* clone();
+			bool isEmpty();
+			void print();
+	};
+#endif 
